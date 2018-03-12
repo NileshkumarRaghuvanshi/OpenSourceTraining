8693b7e67b55548d0a6c7a15981487b73fecb413
Nileshkumar Raghuvanshi <nileshkumar.raghuvanshi@gmail.com>
2018-03-12 23:23:22 +0530

    more changes

--- utFrameworkDemo/test_catch/ut.c
+++ utFrameworkDemo/test_catch/ut.c
@@ -1,14 +1,22 @@
 //#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "../project/find_three_number_avg.h"
-
+#include "../project/find_two_number_avg.h"
 
 TEST_CASE( "Average 3 numbers", "[find_three_number_average]" ) {
     REQUIRE( find_three_number_average(15,15,15) == 15 );
     REQUIRE( find_three_number_average(15,16,17) == 16 );
 }
 
+TEST_CASE( "Average 2 numbers", "[find_two_number_average]" ) {
+    REQUIRE( find_two_number_average(15,15) == 15 );
+    REQUIRE( find_two_number_average(15,17) == 16 );
+}
+
+
+/*
 TEST_CASE( "Average 3 numbers to fail", "[find_three_number_average]" ) {
     REQUIRE( find_three_number_average(15,15,15) == 17 );
     REQUIRE( find_three_number_average(15,16,17) == 18 );
 }
+*/
