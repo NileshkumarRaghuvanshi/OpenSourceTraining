24a60a557e82329eece6a757a03816a37fcff35d
Nileshkumar Raghuvanshi <nileshkumar.raghuvanshi@gmail.com>
2018-03-12 22:06:48 +0530

    added one layer of abstraction with two number avg

--- utFrameworkDemo/find_three_number_avg.c
+++ utFrameworkDemo/find_three_number_avg.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
+#include "find_three_number_avg.h"
 
 int find_three_number_average(int a, int b, int c) {
-	return (a+b+c)/3;
+
+	return (find_two_number_average(find_two_number_average(a,b),c));
 }
---
+++ utFrameworkDemo/find_two_number_avg.c
@@ -0,0 +1,8 @@
+#include "find_two_number_avg.h"
+
+
+int find_two_number_average(int a, int b) {
+
+	return (a + b)/2;
+
+}
