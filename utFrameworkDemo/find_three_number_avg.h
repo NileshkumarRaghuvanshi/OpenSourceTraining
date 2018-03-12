1a5f65c39d1c48d4fba874fad8d8a2e7b7ea61b2
Nileshkumar Raghuvanshi <nileshkumar.raghuvanshi@gmail.com>
2018-03-12 22:13:59 +0530

    removed old file

--- utFrameworkDemo/find_three_number_avg.h
+++
@@ -1,8 +0,0 @@
-#ifndef FIND_AVG_H
-#define FIND_AVG_H
-
-#include "find_two_number_avg.h"
-
-int find_three_number_average(int a, int b, int c);
-
-#endif
--- utFrameworkDemo/find_two_number_avg.h
+++
@@ -1,6 +0,0 @@
-#ifndef TWOAVG_H
-#define TWOAVG_H
-
-int find_two_number_average(int a, int b);
-
-#endif
